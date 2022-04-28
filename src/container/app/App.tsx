6466f2c98f5bf4b67b9f11c419348874daaa413e
vkaniushok <valery.kaniushok@instinctools.com>
2022-04-28 17:17:46 +0300

    feature(master): Setup project

---
+++ src/container/app/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { ColonyClient } from '@colony/colony-js';
+
+import { getColonyClient } from '../../api';
+import Events from '../events';
+
+const App: React.FC = () => {
+  const [colonyClient, setColonyClient] = useState<undefined | ColonyClient>();
+  const [error, setError] = useState<undefined | string>();
+
+  useEffect(() => {
+    getColonyClient()
+      .then(client => {
+        setColonyClient(client);
+      })
+      .catch(() => setError('colony client fetch error'));
+  }, []);
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+  if (!colonyClient) {
+    return <h1>...loading</h1>;
+  }
+  return <Events colonyClient={colonyClient} />;
+};
+
+export default App;
